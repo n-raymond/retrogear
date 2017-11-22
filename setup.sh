1c9dc61d4f0c3d8f3c8d8c0f518da87fdb65be8a
nraymond <nraymond@e-biz.fr>
2017-11-22 16:15:44 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ scp -r . root@recalbox:~/retrogear
 
 ssh -q root@recalbox "
     mount -o remount,rw /boot
-    mount -o remount, rw /recalbox
+    mount -o remount,rw /
     cp retrogear/boot.config /boot/config.txt
     cp retrogear/recalbox.conf recalbox.conf
     cp retrogear/volume-monitor.py /recalbox/scripts/volume-monitor.py
     cp retrogear/custom.sh custom.sh
     rm -Rf retrogear
-    reboot -h
 "
